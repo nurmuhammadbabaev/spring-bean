251ba30264321cca97c42e6064e69c80e9fb9a23
Nurmuhammed <nurmuhammadbabaev@gmail.com>
2022-02-11 11:27:23 +0600

    gagregvw

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -7,8 +7,23 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean==bean2);
+        System.out.println(bean2.getMessage());
+        System.out.println(bean.equals(bean2));
+
+        Cat cat1=applicationContext.getBean("myCat",Cat.class);
+        Cat cat2=applicationContext.getBean("myCat",Cat.class);
+
+        System.out.println("###############################################");
+        System.out.println(cat1);
+        System.out.println(cat1==cat2);
+        System.out.println(cat2);
+        System.out.println(cat1.equals(cat2));
+        System.out.println("###############################################");
+
     }
 }
--- src/main/java/peaksoft/AppConfig.java
+++ src/main/java/peaksoft/AppConfig.java
@@ -2,13 +2,24 @@ package peaksoft;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean
+    @Scope("prototype")
+    public Cat myCat(){
+Cat cat=new Cat();
+cat.setName("Musia");
+cat.setAge(3);
+cat.setColor("White");
+        return cat;
     }
 }
